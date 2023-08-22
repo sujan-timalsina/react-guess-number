aa7018acbb4db87f751d543e99682298b7e13696
sujan-timalsina <sujan.timalsina@apexcollege.edu.np>
2023-08-22 15:57:18 +0545

    . removed from base in vite config file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "./react-guess-number/",
+  base: "/react-guess-number/",
   plugins: [react()],
 })
